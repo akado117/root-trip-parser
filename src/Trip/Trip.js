c04e4cc031529c54a35118f562667f2a7f934afc
akado117 <akaidan@mtu.edu>
2018-11-08 09:22:31 -0500

    Finished implementing Trip class

--- src/Trip/Trip.js
+++ src/Trip/Trip.js
@@ -1,18 +1,24 @@
+// stored here because there's no reason to recreate string ever time parseTime is called
 const partialDateString = 'December 25, 1995 ';
+const timeRegExp = new RegExp('^\\d\\d:\\d\\d$');
 
 class Trip {
-  constructor() {
+  constructor(start, end, distance) {
     // does not store start/end as date objects as unneeded waste of memory
+    if (!timeRegExp.test(start) || !timeRegExp.test(end) || isNaN(distance)) {
+      throw new Error('Please use valid arguments: \\d\\d:\\d\\d, \\d\\d:\\d\\d, number');
+    }
+
+    this._duration = this._parseTime(start, end);
+    this._startTime = start;
+    this._endTime = end;
+    this._distance = distance;
   }
-  
-  getDuration() {}
 
-  getDistance() {}
-
-  parseTime(start, end) {
+  _parseTime(start, end) {
     const startTime = new Date(partialDateString + start);
     const endTime = new Date(partialDateString + end);
-
+    
     const msOfDuration = endTime - startTime;
     
     if (msOfDuration < 0) throw new Error('Please use end times after start times');
@@ -20,6 +26,14 @@ class Trip {
     // ms to minutes
     this._duration = msOfDuration / 60000;
   }
+  
+  getDuration() {
+    return this._duration;
+  }
+
+  getDistance() {
+    return this._distance;
+  }  
 }
 
 module.exports = Trip;
--- src/Trip/Trip.test.js
+++ src/Trip/Trip.test.js
@@ -3,37 +3,49 @@ const Trip = require('./Trip');
 describe('Trip Class', () => {
   let trip;
   beforeEach(() => {
-    trip = new Trip('12:30', '01:30', 40);
+    trip = new Trip('12:30', '13:30', 40);
   });
   describe('constructor', () => {
     test('should throw an error if format of args is invalid', () => {
       const errorString = 'Please use valid arguments: \\d\\d:\\d\\d, \\d\\d:\\d\\d, number';
       try {
-        trip.constructor('12:30', '01:30', 'asd');
+        new Trip('12:30', '01:30', 'asd');
+        // Should not hit this because above should throw error
+        expect(5).toEqual(6);
       } catch (e) {
         expect(e.message).toEqual(errorString);
       }
       try {
-        trip.constructor('12:30', '1:30', 1);
+        new Trip('12:30', '1:30', 1);
+        expect(5).toEqual(6);
       } catch (e) {
         expect(e.message).toEqual(errorString);
       }
       try {
-        trip.constructor(32, '01:30', 1);
+        new Trip(32, '01:30', 1);
+        expect(5).toEqual(6);
       } catch (e) {
         expect(e.message).toEqual(errorString);
       }
     });
     test('should call parseTime and save start/end time, duration, and distance to trip object', () => {
-      trip.parseTime = jest.fn(() => 50);
+      const parseTimeHolder = Trip.prototype._parseTime;
+      // Modifying prototype can be dangerous. Try catch to make sure prototype is restored before test completed
+      try {
+        Trip.prototype._parseTime = jest.fn(() => 50);
+        
+        trip = new Trip('07:30', '08:30', 45);
 
-      trip.constructor('07:30', '08:30', 45);
-
-      expect(trip._duration).toEqual(50);
-      expect(trip._startTime).toEqual('07:30');
-      expect(trip._endTime).toEqual('08:30');
-      expect(trip._distance).toEqual(45);
-      expect(trip.parseTime).toHaveBeenLastCalledWith('07:30', '08:30');
+        expect(trip._duration).toEqual(50);
+        expect(trip._startTime).toEqual('07:30');
+        expect(trip._endTime).toEqual('08:30');
+        expect(trip._distance).toEqual(45);
+        expect(trip._parseTime).toHaveBeenLastCalledWith('07:30', '08:30');        
+        Trip.prototype._parseTime = parseTimeHolder;
+      } catch (e) {
+        Trip.prototype._parseTime = parseTimeHolder;
+        throw new Error(e);
+      }
     });
   });
   describe('getDuration', () => {
@@ -52,22 +64,22 @@ describe('Trip Class', () => {
       expect(trip.getDistance()).toEqual(distance);
     });
   });
-  describe('parseTime', () => {
+  describe('_parseTime', () => {
     test('should throw error if end time is before start time', () => {
       try {
-        trip.parseTime('12:30', '3:30');
-        // should never run because error
+        trip._parseTime('12:30', '03:30');
+        // Should never run because error
         expect(5).toBe(6);
       } catch (e) {
         expect(e.message).toEqual('Please use end times after start times');
       }
     });
     test('should parse start/end time when its a simple hour subtraction', () => {
-      trip.parseTime('12:30', '13:30');
+      trip._parseTime('12:30', '13:30');
       expect(trip._duration).toEqual(60);
     });
     test('should parse start/end time when its hour and minute subtraction', () => {
-      trip.parseTime('12:30', '13:15');
+      trip._parseTime('12:30', '13:15');
       expect(trip._duration).toEqual(45);
     });
   });

348399800f6f9c82acc86dd211783250a6982f5d
akado117 <akaidan@mtu.edu>
2018-11-12 08:10:26 -0500

    added comment to app.js

--- app.js
+++ app.js
@@ -2,20 +2,8 @@ const fs = require('fs');
 const path = require('path');
 const DriverController = require('./src/Controllers/DriverController');
 
-const encoding = 'utf-8';
-
-/* File Data Ops */
-const inputPath = process.argv[2];
-if (typeof inputPath !== 'string') return console.log('Please include a path to your file')
-
-let filePath = path.join(__dirname, inputPath);
-
-let fileData = fs.readFileSync(filePath, encoding);
-// Check if user is passing in an absolute path
-if (!fileData) fileData = fs.readFileSync(inputPath, encoding);
-if (!fileData) return console.log('File not found. Please verify your path to file (accepts absolute and relative)');
-
-// Using regex to make end of line OS agnostic
+function mainApp(fileData) {
+  // Using regex to make end of line OS agnostic
 const splitFileRegex = new RegExp('\r\n|\r|\n', 'g');
 const fileLines = fileData.split(splitFileRegex);
 
@@ -41,4 +29,43 @@ for (const driver of consolidatedDrivers) {
   const distance = Math.round(driver.getTotalDistance());
   const speed = Math.round(driver.getAverageSpeed());
   console.log(`${name}: ${distance} miles ${speed ? `@ ${speed} mph` : '' }`)
+}
+}
+
+
+
+
+const encoding = 'utf-8';
+
+let fileData
+/* File Data Ops */
+//is terminal or piped in data
+if (process.stdin.isTTY) {
+  const inputPath = process.argv[2];
+
+  // File not passed in
+  if (typeof inputPath !== 'string') return console.log('Please include a path to your file')
+
+  let filePath = path.join(__dirname, inputPath);
+
+  fileData = fs.readFileSync(filePath, encoding);
+  // Check if user is passing in an absolute path
+  if (!fileData) fileData = fs.readFileSync(inputPath, encoding);
+  if (!fileData) return console.log('File not found. Please verify your path to file (accepts absolute and relative)');
+  mainApp(fileData)
+} else {
+  fileData = '';
+  process.stdin.setEncoding(encoding);
+  process.stdin.on('readable', function() {
+    let chunk;
+    while (chunk = process.stdin.read()) {
+      fileData += chunk;
+    }
+  });
+ 
+  process.stdin.on('end', function () {
+    // There will be a trailing \n from the user hitting enter. Get rid of it.
+    fileData = fileData.replace(/\n$/, '');
+    mainApp(fileData);
+  });
 }
